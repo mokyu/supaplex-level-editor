685f716819c98b3966ce41fd2d62ca54199cfa2e
Mokyu <mokyu@users.noreply.github.com>
2019-06-25 00:26:33 +0200

    Fixed issue where in some cases loading a level collection does nothing

--- src/mokyu/supaplexLevelEditor/Information.java
+++ src/mokyu/supaplexLevelEditor/Information.java
@@ -21,5 +21,5 @@ package mokyu.supaplexLevelEditor;
  * @author Mokyu
  */
 public class Information {
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 }
